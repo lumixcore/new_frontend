d7aa04f0ebcddf833b34cabac358349d2f557586
AlphastT101 <alphast.1001@gmail.com>
2026-01-31 13:34:04 +0800

    adsense

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2804720115610064"
+     crossorigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
