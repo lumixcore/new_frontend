0944d257966381c6eb4efecc8684781ec5dc2e99
AlphastT101 <alphast.1001@gmail.com>
2026-05-26 21:38:09 +0600

    nuke

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  images: {
-    unoptimized: true, // Required for static export
-  },
+  output: 'export',
   allowedDevOrigins: ['landing.lumixcore.com'],
 };
 
